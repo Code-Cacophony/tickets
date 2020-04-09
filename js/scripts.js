e11db4a94668928b0f78d788fee36907adc2f8fd
Code-Cacophony <57301331+Code-Cacophony@users.noreply.github.com>
2020-04-09 13:56:21 -0700

    add prototypes and values

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,37 @@ function Ticket(movie, time, age) {
   this.age = age;
 }
 
+Ticket.prototype.calculatePrice = function () {
+
+
+}
+
+Ticket.prototype.displayTicket = function () {
+
+}
+
 
 
 $(document).ready(function () {
-  $().submit(function (event) {
+  $("#user-input").submit(function (event) {
     event.preventDefault();
 
-    var ticket = new Ticket()
+    var movie = parseInt($("#movie").val());
+    var time = parseInt($("#time").val());
+    var age = parseInt($("#age").val());
+
+    var ticket = new Ticket(movie, time, age);
+
+    console.log(movie);
+    console.log(time);
+    console.log(age);
+    console.log(ticket);
 
 
-  })
+  });
 
 
 });
+
+//Create a ticket that includes movie, time, age, and price
+//Calculate price and display the ticket
